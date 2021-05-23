0110dd30206cf37f24742f2d447256800881e275
Ben Smith <8enSmith@users.noreply.github.com>
2021-05-23 23:57:57 +0100

    Use input instead of slider for setting timeout (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import React, { useState } from 'react';
 import './App.css';
 import Board from './engine/board';
 import Grid from './Grid';
-import SimpleRange from './range';
 import { useInterval } from './useInterval';
 
-const DEFAULT_TIMEOUT_INTERVAL = 2500;
+const DEFAULT_TIMEOUT_INTERVAL = 1000;
 
 const _board = new Board(15, 15);
 
@@ -30,7 +29,7 @@ const App = () => {
   const [board, setBoard] = useState(_board);
   const [isRunning, setIsRunning] = useState(true);
   const [iteration, setIteration] = useState(1);
-  const [timeoutInterval, setTimeoutInterval] = useState<number | null>(
+  const [timeoutInterval, setTimeoutInterval] = useState<number>(
     DEFAULT_TIMEOUT_INTERVAL
   );
 
@@ -51,6 +50,7 @@ const App = () => {
         <button
           onClick={() => {
             setIsRunning(false);
+            setTimeoutInterval(DEFAULT_TIMEOUT_INTERVAL);
             setBoard(_board);
             setIteration(1);
           }}
@@ -59,7 +59,15 @@ const App = () => {
         </button>
         <Grid board={board} updateBoard={setBoard} />
         <div>{`Iteration ${iteration}`}</div>
-        <SimpleRange setTimeoutInterval={setTimeoutInterval} />
+        <input
+          type="number"
+          value={timeoutInterval}
+          min={0}
+          max={2500}
+          onInput={(event) => {
+            setTimeoutInterval(parseInt(event.currentTarget.value, 10) || 0);
+          }}
+        />
       </header>
     </div>
   );
