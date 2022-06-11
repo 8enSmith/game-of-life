2dd0418461c7c17fa3bc8cf10273d051ff7effd3
Ben Smith <v-ben.smith@zoopla.co.uk>
2022-06-11 20:50:09 +0100

    Fix deprecated render in beforeEach isssue

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,26 +3,32 @@ import userEvent from '@testing-library/user-event'
 import App from '../App';
 
 describe('When App renders', () => {
-  beforeEach(() => render(<App />));
-
   describe('Should correctly render', () => {
     it('table', async () => {
+      render(<App />)
+
       expect(await screen.findByTestId('table')).toBeVisible();
     });
 
     it('correct iteration value', async () => {
+      render(<App />)
+      
       expect(await screen.findByText('Iteration 0')).toBeTruthy();
     });
   });
 
   describe('The button', () => {
     it('stop should work as expected', async () => {
+      render(<App />)
+      
       const user = userEvent.setup()
       await user.click(screen.getByText('Start'));
       expect(screen.getByText('Pause')).toBeInTheDocument();
     });
 
     it('reset should work as expected', async () => {
+      render(<App />)
+      
       const user = userEvent.setup()
       await user.click(screen.getByText('Start'));
 
@@ -36,12 +42,16 @@ describe('When App renders', () => {
 
   describe('the time interval input', () => {
     it('should work as expected', async () => {
+      render(<App />)
+      
       const input = screen.getByLabelText('timeout-interval');
       fireEvent.input(input, { target: { value: '500' } });
       expect((input as HTMLInputElement).value).toBe('500');
     });
 
     it('should display zero when slider moved to minimum value', async () => {
+      render(<App />)
+      
       const input = screen.getByLabelText('timeout-interval');
       fireEvent.input(input, { target: { value: '0' } });
       expect((input as HTMLInputElement).value).toBe('0');
@@ -50,6 +60,8 @@ describe('When App renders', () => {
 
   describe('Clicking on a dead cell in the grid', () => {
     it('should bring it back to life', async () => {
+      render(<App />)
+      
       const deadCells = await screen.findAllByLabelText(/dead/);
       const sut = deadCells[0];
       await userEvent.click(sut);
